fc92e4e40712abc21a3b9807f1faaf3bcddf5e8c
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2020-11-28 17:25:36 +0530

    Tutorial 6 Taking user input

--- src/apples.java
+++ src/apples.java
@@ -1,12 +1,11 @@
 package src;
 
+import java.util.Scanner;
+
 public class apples {
     public static void main(String[] args){
-        double tuna;
-        tuna = 5.28;
-        System.out.print("I want ");
-        System.out.print(tuna);
-        System.out.println(" Tunas");
-        System.out.println("Apples");
+        System.out.print("Enter your line: ");
+        Scanner bucky = new Scanner(System.in);
+        System.out.println(bucky.nextLine());
     }
 }
