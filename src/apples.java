4d93540bea2fdbabf9ec099fd5a690618a7b0e56
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2020-11-30 00:27:35 +0530

    Tutorial 9: Increment operators

--- src/apples.java
+++ src/apples.java
@@ -1,29 +1,54 @@
 package src;
 
-public class apples {
+public class apples{
     public static void main(String[] args){
-        int girls, boys, people;
+        int tuna;
+        tuna = 5;
+        System.out.println("tuna: "+tuna);
 
-        System.out.println("=========================");
-        girls = 6;
-        boys = 3;
-        System.out.println("girls = "+girls+" | boys = "+boys);
-        people = girls + boys;
-        System.out.println("Add: "+people);
-        people = girls - boys;
-        System.out.println("Subtract: "+people);
-        people = girls * boys;
-        System.out.println("Multiply: "+people);
+        System.out.println("===========================");
+        System.out.println("pre-increment (increment the variable before it gets used)");
+        System.out.println(++tuna);
+        System.out.println(tuna);
+        System.out.println("post-increment (increment the variable after it gets used)");
+        tuna = 5; // reset tuna to 5
+        System.out.println(tuna++);
+        System.out.println(tuna);
 
-        // Changing the value of girls for understanding un-even division
-        System.out.println("=========================");
-        girls = 11;
-        System.out.println("girls = "+girls+" | boys = "+boys);
-        people = girls / boys;
-        System.out.println("Integer division: "+people);
-        // Note that you don't get decimal part in the above output.
-        // This is because you have set the type for variable "people" as "int"
-        people = girls % boys;
-        System.out.println("Modulus: "+people);
+        System.out.println("===========================");
+        System.out.println("post-decrement (decrement the variable before it gets used)");
+        tuna = 5; // reset tuna to 5
+        System.out.println(--tuna);
+        System.out.println(tuna);
+        System.out.println("post-decrement (decrement the variable after it gets used)");
+        tuna = 5; // reset tuna to 5
+        System.out.println(tuna--);
+        System.out.println(tuna);
+
+        System.out.println("===========================");
+        System.out.println("Addition-assignment (adding 3 to the variable tuna)");
+        tuna = 5; // reset tuna to 5
+        tuna += 3;
+
+        System.out.println(tuna);
+        System.out.println("Subtraction-assignment (subtracting 3 from the variable tuna)");
+        tuna = 5; // reset tuna to 5
+        tuna -= 3;
+        System.out.println(tuna);
+
+        System.out.println("Multiplication-assignment (multiplying 3 to the variable tuna)");
+        tuna = 5; // reset tuna to 5
+        tuna *= 3;
+        System.out.println(tuna);
+
+        System.out.println("Division-assignment (dividing the variable tuna by 3)");
+        tuna = 5; // reset tuna to 5
+        tuna /= 3;
+        System.out.println(tuna);
+
+        System.out.println("Modulus-assignment (getting modulus of the variable tuna by 3)");
+        tuna = 5; // reset tuna to 5
+        tuna %= 3;
+        System.out.println(tuna);
     }
 }
