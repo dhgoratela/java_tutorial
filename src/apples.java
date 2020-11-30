220c053614426909882c3c2d2a5e37131612ac6d
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2020-11-30 21:49:46 +0530

    Tutorial 12: Switch Statement

--- src/apples.java
+++ src/apples.java
@@ -1,53 +1,28 @@
 package src;
 
+import java.util.Scanner;
+
 public class apples{
     public static void main(String[] args){
-        // Dating Website
-        int girl, boy;
+        Scanner bucky = new Scanner(System.in);
+        int age;
 
-        System.out.println("======= Logical 1 =======");
-        boy = 18;
-        girl = 68;
-        if(boy>10){
-            System.out.println("You can enter");
-        }else{
-            System.out.println("You are too young");
-        }
+        System.out.print("Enter age: ");
+        age = bucky.nextInt();
 
-        System.out.println("======= Logical 2 =======");
-        boy = 18;
-        girl = 68;
-        if(boy>10 && girl<60){
-            System.out.println("You can enter");
-        }else{
-            System.out.println("You cannot enter");
-        }
-
-        System.out.println("======= Logical 3 =======");
-        boy = 18;
-        girl = 40;
-        if(boy>10 && girl<60){
-            System.out.println("You can enter");
-        }else{
-            System.out.println("You cannot enter");
-        }
-
-        System.out.println("======= Logical 4 =======");
-        boy = 18;
-        girl = 99;
-        if(boy>10 || girl<60){
-            System.out.println("You can enter");
-        }else{
-            System.out.println("You cannot enter");
-        }
-
-        System.out.println("======= Logical 5 =======");
-        boy = 8;
-        girl = 99;
-        if(boy>10 || girl<60){
-            System.out.println("You can enter");
-        }else{
-            System.out.println("You cannot enter");
+        switch(age){
+            case 1:
+                System.out.println("You can crawl");
+                break;
+            case 2:
+                System.out.println("You can talk");
+                break;
+            case 3:
+                System.out.println("You are in toruble");
+                break;
+            default:
+                System.out.println("I don't know how old you are");
+            break;
         }
     }
 }
