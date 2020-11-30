97c60d45bdd7914b5832b552368b921e6f9f22ad
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2020-11-30 22:12:05 +0530

    Tutorial 13: While loop

--- src/apples.java
+++ src/apples.java
@@ -1,28 +1,11 @@
 package src;
 
-import java.util.Scanner;
-
 public class apples{
     public static void main(String[] args){
-        Scanner bucky = new Scanner(System.in);
-        int age;
-
-        System.out.print("Enter age: ");
-        age = bucky.nextInt();
-
-        switch(age){
-            case 1:
-                System.out.println("You can crawl");
-                break;
-            case 2:
-                System.out.println("You can talk");
-                break;
-            case 3:
-                System.out.println("You are in toruble");
-                break;
-            default:
-                System.out.println("I don't know how old you are");
-            break;
+        int counter = 0;
+        while(counter < 10){
+            System.out.println(counter);
+            counter++;
         }
     }
 }
