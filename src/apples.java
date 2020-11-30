6ada2c42f2928c8a6c5d29766824304da3e518f7
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2020-11-30 21:32:16 +0530

    Tutorial 11: Logical operators

--- src/apples.java
+++ src/apples.java
@@ -2,12 +2,52 @@ package src;
 
 public class apples{
     public static void main(String[] args){
-        int test = 6;
-        // Comparison operators: ==, !=, <, >, <= and >=
-        if (test == 9){
-            System.out.println("Yes");
+        // Dating Website
+        int girl, boy;
+
+        System.out.println("======= Logical 1 =======");
+        boy = 18;
+        girl = 68;
+        if(boy>10){
+            System.out.println("You can enter");
         }else{
-            System.out.println("this is else");
+            System.out.println("You are too young");
+        }
+
+        System.out.println("======= Logical 2 =======");
+        boy = 18;
+        girl = 68;
+        if(boy>10 && girl<60){
+            System.out.println("You can enter");
+        }else{
+            System.out.println("You cannot enter");
+        }
+
+        System.out.println("======= Logical 3 =======");
+        boy = 18;
+        girl = 40;
+        if(boy>10 && girl<60){
+            System.out.println("You can enter");
+        }else{
+            System.out.println("You cannot enter");
+        }
+
+        System.out.println("======= Logical 4 =======");
+        boy = 18;
+        girl = 99;
+        if(boy>10 || girl<60){
+            System.out.println("You can enter");
+        }else{
+            System.out.println("You cannot enter");
+        }
+
+        System.out.println("======= Logical 5 =======");
+        boy = 8;
+        girl = 99;
+        if(boy>10 || girl<60){
+            System.out.println("You can enter");
+        }else{
+            System.out.println("You cannot enter");
         }
     }
 }
