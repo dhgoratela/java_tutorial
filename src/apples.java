f8675b92ffe3fd2135058d05ff8ad2c06a71af54
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2020-12-04 00:36:41 +0530

    Tutorial 19: else if Statement

--- src/apples.java
+++ src/apples.java
@@ -1,17 +1,20 @@
 package src;
+import java.util.Scanner;
 
 public class apples{
     public static void main(String[] args){
-        int age = 60;
-        if (age < 50){
-            System.out.println("You are young");
-        }else{
-            System.out.println("You are old");
-            if(age > 75){
-                System.out.println("You are REALLY old!");
-            }else{
-                System.out.println("dont worry you are not really thta old");
-            }
-        }
+
+         Scanner input = new Scanner(System.in);
+         System.out.println("Enter your age: ");
+         int age = input.nextInt();
+
+         if (age>=60)
+             System.out.println("You ae a Senior citizen");
+         else if (age>=50)
+             System.out.println("You are in your 50's");
+         else if (age>=40)
+             System.out.println("You are in your 40's");
+         else
+             System.out.println("You are a young buck");
     }
 }
