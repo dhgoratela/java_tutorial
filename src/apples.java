485e185f3fe91436548eaecaca8de938f849c397
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2020-12-04 00:43:48 +0530

    Tutorial 20: conditional operator

--- src/apples.java
+++ src/apples.java
@@ -1,20 +1,9 @@
 package src;
-import java.util.Scanner;
 
 public class apples{
     public static void main(String[] args){
+        int age = 21;
 
-         Scanner input = new Scanner(System.in);
-         System.out.println("Enter your age: ");
-         int age = input.nextInt();
-
-         if (age>=60)
-             System.out.println("You ae a Senior citizen");
-         else if (age>=50)
-             System.out.println("You are in your 50's");
-         else if (age>=40)
-             System.out.println("You are in your 40's");
-         else
-             System.out.println("You are a young buck");
+        System.out.println(age > 50 ? "You are old": "You are Young");
     }
 }
