b56e263ab89062eaded6881e8bcd0f7e7f21e875
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2020-12-06 00:07:10 +0530

    Tutorial 21: Simple Averaging Program

--- src/apples.java
+++ src/apples.java
@@ -1,9 +1,21 @@
 package src;
+import java.util.Scanner;
 
 public class apples{
     public static void main(String[] args){
-        int age = 21;
+        int total = 0;
+        int grade;
+        int average;
+        int counter = 0;
+        Scanner input = new Scanner(System.in);
 
-        System.out.println(age > 50 ? "You are old": "You are Young");
+        System.out.println("Enter the 10 numbers:");
+        while (counter < 10){
+            grade = input.nextInt();
+            total += grade;
+            counter++;
+        }
+        average = total/10;
+        System.out.println("Your average is: "+average);
     }
 }
