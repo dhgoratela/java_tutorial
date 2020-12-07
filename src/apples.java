d542757e85ae25cb20d01037292260bb59729407
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2020-12-07 23:51:34 +0530

    Tutorial 25: Math Class Methods

--- src/apples.java
+++ src/apples.java
@@ -2,11 +2,13 @@ package src;
 
 public class apples{
     public static void main(String[] args){
-        int counter = 0; // try number 10 and 15 also
-        // do-while executes at least once before checking the condition
-        do{
-            System.out.println(counter);
-            counter++;
-        }while(counter<=10);
+        // Built-in math functions. Notice that you don't need to import Math class
+        System.out.println("Absolute value of -9.23 is: "+Math.abs(-9.23));
+        System.out.println("Ceil rounds up 5.21 to: "+Math.ceil(5.21));
+        System.out.println("Floor rounds down 8.9 to: "+Math.floor(8.9));
+        System.out.println("Maximum out of 2.2 and 6.2 is: "+Math.max(2.2, 6.2));
+        System.out.println("Minimum out of 2.2 and 6.2 is: "+Math.min(2.2, 6.2));
+        System.out.println("Square root of 144 is: "+Math.sqrt(144));
+        System.out.println("5 to the power 3 is: "+Math.pow(5,3));
     }
 }
