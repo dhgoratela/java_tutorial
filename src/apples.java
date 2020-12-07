418bd7cf54fcb9c48555dc395e52c06e68e679f5
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2020-12-08 00:07:02 +0530

    Tutorial 26: Random Number Generator

--- src/apples.java
+++ src/apples.java
@@ -1,14 +1,17 @@
 package src;
+import java.util.Random;
 
 public class apples{
     public static void main(String[] args){
-        // Built-in math functions. Notice that you don't need to import Math class
-        System.out.println("Absolute value of -9.23 is: "+Math.abs(-9.23));
-        System.out.println("Ceil rounds up 5.21 to: "+Math.ceil(5.21));
-        System.out.println("Floor rounds down 8.9 to: "+Math.floor(8.9));
-        System.out.println("Maximum out of 2.2 and 6.2 is: "+Math.max(2.2, 6.2));
-        System.out.println("Minimum out of 2.2 and 6.2 is: "+Math.min(2.2, 6.2));
-        System.out.println("Square root of 144 is: "+Math.sqrt(144));
-        System.out.println("5 to the power 3 is: "+Math.pow(5,3));
+        Random dice = new Random();
+        int number;
+
+        for (int counter=0; counter<=10; counter++){
+            number = 1 + dice.nextInt(6);
+            // The argument (6) signifies the boundary.
+            // Here, it means consider (first six) random numbers starting from (zero).
+            // We add (one) because random numbers are generated from 0 to 5. So we push the generated number by 1.
+            System.out.println(number);
+        }
     }
 }
