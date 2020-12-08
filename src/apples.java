9578a97f56db360614c2ce7e81835af3521b25f1
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2020-12-09 00:02:44 +0530

    Tutorial 29: Summing Elements of Arrays

--- src/apples.java
+++ src/apples.java
@@ -2,12 +2,12 @@ package src;
 
 public class apples{
     public static void main(String[] args){
-        int[] bucky = {32, 12, 18, 54, 2};
-
-        System.out.println("Index\tValue");
+        int[] bucky = {21, 16, 86, 21, 3};
+        int sum = 0;
 
         for(int counter=0; counter<bucky.length; counter++){
-            System.out.println(counter+"\t"+bucky[counter]);
+            sum += bucky[counter];
         }
+        System.out.println("The sum of theze numbers is " + sum);
     }
 }
