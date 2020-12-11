3763e678feed9681a012f41b8cf72c1bbce9e92b
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2020-12-11 23:10:47 +0530

    Tutorial 31: Enhanced for Loop

--- src/apples.java
+++ src/apples.java
@@ -1,24 +1,14 @@
 package src;
-import java.util.Random;
 
-public class apples{
+public class apples {
     public static void main(String[] args){
-        int[] freq = new int[7]; // initialize result array (Which will bear frequencies of all face values)
-        Random rand = new Random();
+        int[] bucky = {3, 4, 5, 6, 7};
+        int total = 0;
 
-        // Roll the dice 1000 times
-        // Generate random number between 0 to 5. And push the generated number by 1 to bring it in range of 1 to 6
-        // This modified random number will be our index which will denote the face value of dice
-        // Increment the value contained in that index by 1. This increment will record the frequency of that face value
-        for(int roll=0; roll<1000; roll++){
-            ++freq[1+rand.nextInt(6)];
+        for (int x: bucky){
+            total+=x;
         }
 
-        // Display frequencies for each face value
-        System.out.println("Face\tFrequency");
-        for(int face=1; face<freq.length; face++){
-            System.out.println(face+"\t"+freq[face]);
-        }
-
+        System.out.println(total);
     }
 }
