dfa86c0023afb36b7c6ce73c5611aac2f9ef93d5
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2020-12-12 19:50:05 +0530

    Tutorial 34: Table for Multi Arrays

--- src/apples.java
+++ src/apples.java
@@ -4,5 +4,18 @@ public class apples {
     public static void main(String[] args){
         int[][] firstArray = {{8, 9, 10, 11}, {12, 13, 14, 15}};
         int[][] secondArray = {{30, 31, 32, 33}, {43}, {4, 5, 6}};
+        System.out.println("This is the firts array:");
+        display(firstArray);
+        System.out.println("This is the second array:");
+        display(secondArray);
+    }
+
+    public static void display(int[][] x){
+        for(int row=0; row<x.length; row++){
+            for (int col=0; col<x[row].length; col++){
+                System.out.print(x[row][col] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
