7fb5383a7439c2463651d0bcfcf2503ee9e1bdfd
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2020-12-12 22:17:51 +0530

    Tutorial 37: Display Regular time

--- src/apples.java
+++ src/apples.java
@@ -1,11 +1,12 @@
 package src;
 
 public class apples {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         tuna tunaObject = new tuna();
         System.out.println(tunaObject.toMilitary());
 
-        tunaObject.setTime(13, 27, 6);
+        tunaObject.setTime(12,0,0);
         System.out.println(tunaObject.toMilitary());
+        System.out.println(tunaObject.toRegularTime());
     }
 }
--- src/tuna.java
+++ src/tuna.java
@@ -1,19 +1,23 @@
 package src;
 
 public class tuna {
-    // Java auto initializes the instance variables to 0 or empty string (Depending on the datatype)
-    // if the initial value is not provided
     private int hour;
     private int minutes;
     private int seconds;
 
-    public void setTime(int h,int m, int s) {
+    public void setTime(int h, int m, int s) {
         hour = ((h>=0 && h<24) ? h: 0);
-        minutes = ((m>=0 && m<60) ? m:0);
+        minutes = ((m>=0 && m<60) ? m: 0);
         seconds = ((s>=0 && s<60) ? s: 0);
     }
 
     public String toMilitary() {
         return String.format("%02d:%02d:%02d", hour, minutes, seconds);
+    }
+
+    public String toRegularTime() {
+        return String.format("%d:%02d:%02d %s",
+                ((hour==0 || hour==12 || hour==24) ? 12: hour%12),
+                minutes, seconds, (hour>=12 ? "PM": "AM"));
     }
 }
