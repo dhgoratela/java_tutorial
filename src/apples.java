c003de095705e7b94ac1ed49eeb5ec25305e87b2
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2020-12-23 23:12:41 +0530

    Tutorial 42: toString (overriding Object class in java.lang)

--- src/apples.java
+++ src/apples.java
@@ -1,15 +1,7 @@
 package src;
 
 public class apples {
-    public static void main(String[] args) {
-        tuna tunaObject = new tuna();
-        tuna tunaObject2 = new tuna(5);
-        tuna tunaObject3 = new tuna(5, 13);
-        tuna tunaObject4 = new tuna(5, 13, 43);
-
-        System.out.println(tunaObject.toMilitary());
-        System.out.println(tunaObject2.toMilitary());
-        System.out.println(tunaObject3.toMilitary());
-        System.out.println(tunaObject4.toMilitary());
+    public static void main(String[] args){
+        potpie potpieObject = new potpie(4,5,6);
     }
 }
