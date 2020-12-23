ea8b4ade6d0d9aad5533b3ff731b358134458c81
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2020-12-23 23:58:25 +0530

    Tutorial 43: Composition

--- src/apples.java
+++ src/apples.java
@@ -1,7 +1,9 @@
 package src;
 
 public class apples {
-    public static void main(String[] args){
-        potpie potpieObject = new potpie(4,5,6);
+    public static void main (String[] args) {
+        potpie potpieObject = new potpie(4, 5, 6);
+        tuna tunaObject = new tuna("Bucky", potpieObject);
+        System.out.println(tunaObject);
     }
 }
